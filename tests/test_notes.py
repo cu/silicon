4e96a2bf66145372a85ecb7a8d2c2f53f0b98b20
Charles Ulrich <charles@bityard.net>
2022-03-19 22:08:53 -0400

    Add test for table of contents generation

--- tests/test_notes.py
+++ tests/test_notes.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+from pathlib import Path
+from textwrap import dedent
 
 from bs4 import BeautifulSoup
 import pytest
@@ -198,3 +200,15 @@ def test_search_body_plaintext(client):
     r = client.get("/search?query=bravo")
     body = _page(r.data).find(class_="body-matches").dd.get_text()
     assert body == "alpha bravo charlie"
+
+
+def test_table_of_contents(client):
+    """Generation of a table of contents for a page."""
+
+    toc_in = Path(__file__).parent / "toc_in.md"
+    toc_out = Path(__file__).parent / "toc_out.html"
+    with toc_in.open() as f:
+        client.post("/edit/test", data={"body": f.read()})
+    r = client.get("/htmx/toc/test")
+    with toc_out.open() as f:
+        assert r.data.decode("utf-8") == f.read()
---
+++ tests/toc_in.md
@@ -0,0 +1,21 @@
+Some leading text.
+
+### Start at heading level 3
+
+Normally headings start at 1, but there's no law that says headings can't
+start at some lower level.
+
+# Heading 1
+## Heading 2
+### Heading 3
+#### Heading 4
+##### Heading 5
+###### Heading 6
+
+# Heading with _emphasis_ and **more empahasis**
+
+The inline styles on these should get removed.
+
+# Heading with <span>html</span>
+
+HTML in headings should be escaped.
